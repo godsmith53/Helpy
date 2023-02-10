29fece88a52fb5f4102fd9a045158880e1e263b4
Aditya Verma <toaditya07@gmail.com>
2023-02-10 17:41:25 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState,useEffect } from 'react';
+import { View, Image } from 'react-native';
+import MapView, { Marker, Circle } from 'react-native-maps';
+import * as Location from 'expo-location';
 
-export default function App() {
+const DelhiMap = () => {
+  const [markers, setMarkers] = useState([
+    {
+      latitude: 28.6129,
+      longitude: 77.2295,
+      title: 'India Gate',
+      description: 'India Gate is a war memorial located in New Delhi, India.',
+    },
+    {
+      latitude: 28.5355,
+      longitude: 77.2410,
+      title: 'Qutub Minar',
+      description: 'Qutub Minar is a minaret located in New Delhi, India.',
+    },
+    {
+      latitude: 28.5456,
+      longitude: 77.1907,
+      title: 'IIT Delhi',
+      description: 'IIT Delhi is a public technical and research university located in New Delhi, India.',
+    },
+  ]);
+
+  const center = {
+    latitude: (markers[0].latitude ) ,
+    longitude: (markers[0].longitude ) ,
+  };
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+
+      
+    })();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: center.latitude,
+        longitude: center.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      showsUserLocation={true}
+    >
+      <Circle
+        center={center}
+        radius={5000}
+        strokeWidth={2}
+        strokeColor="#1a66ff"
+        fillColor="rgba(0, 255, 0, 0.25)"
+      />
+      {markers.map((marker) => (
+        <Marker
+          key={marker.title}
+          coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+          title={marker.title}
+          description={marker.description}
+        >
+          <Image
+            source={require('./assets/repairavatar.png')}
+            style={{ height: 40, width: 40 }}
+          />
+        </Marker>
+      ))}
+    </MapView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default DelhiMap;
