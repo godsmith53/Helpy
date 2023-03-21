26b448f99a785b26a0ad9016faf6bfc6821db8f7
Aditya Verma <toaditya07@gmail.com>
2023-03-21 19:15:36 +0530

    modified:   App.js 	modified:   package-lock.json 	modified:   package.json

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image } from 'react-native';
 import MapView, { Marker, Circle } from 'react-native-maps';
 import * as Location from 'expo-location';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 const DelhiMap = () => {
   const [markers, setMarkers] = useState([
@@ -24,15 +25,15 @@ const DelhiMap = () => {
       description: 'IIT Delhi is a public technical and research university located in New Delhi, India.',
     },
   ]);
+ 
 
   const center = {
-    latitude: (markers[0].latitude ) ,
-    longitude: (markers[0].longitude ) ,
+    latitude: markers[0].latitude,
+    longitude: markers[0].longitude,
   };
 
   useEffect(() => {
     (async () => {
-      
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
@@ -41,43 +42,88 @@ const DelhiMap = () => {
 
       let location = await Location.getCurrentPositionAsync({});
       console.log(location);
-
-      
     })();
   }, []);
 
   return (
-    <MapView
-      style={{ flex: 1 }}
-      initialRegion={{
-        latitude: center.latitude,
-        longitude: center.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-      showsUserLocation={true}
-    >
-      <Circle
-        center={center}
-        radius={5000}
-        strokeWidth={2}
-        strokeColor="#1a66ff"
-        fillColor="rgba(0, 255, 0, 0.25)"
+    <View style={{ flex: 1 }}>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        fetchDetails={true}
+        GooglePlacesSearchQuery={{
+          rankby:"distance",
+
+        }}
+        onPress={(data, details = null) => {
+          console.log(data, details);
+          if (details) { // make sure details is not null or undefined
+            setMarkers([
+              {
+                latitude: details.geometry.location.lat,
+                longitude: details.geometry.location.lng,
+                title: data.structured_formatting.main_text,
+                description: data.description,
+              },
+            ]);
+          }
+        }}
+        query={{
+          key: 'API_KEY',
+          language: 'en',
+          components: 'country:in',
+          fields: 'geometry',
+          
+        }}
+        styles={{
+          container: {
+            flex: 0,
+          },
+          textInput: {
+            backgroundColor: '#FFFFFF',
+            borderRadius: 5,
+            fontSize: 16,
+            paddingVertical: 10,
+            paddingHorizontal: 20,
+            marginHorizontal: 20,
+            marginTop: 20,
+          },
+          poweredContainer: {
+            display: 'none',
+          },
+        }}
+        enablePoweredByContainer={false}
       />
-      {markers.map((marker) => (
-        <Marker
-          key={marker.title}
-          coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
-          title={marker.title}
-          description={marker.description}
-        >
-          <Image
-            source={require('./assets/repairavatar.png')}
-            style={{ height: 40, width: 40 }}
-          />
-        </Marker>
-      ))}
-    </MapView>
+
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: center.latitude,
+          longitude: center.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}
+      >
+        <Circle
+          center={center}
+          radius={5000}
+          strokeWidth={2}
+          strokeColor="#1a66ff"
+          fillColor="rgba(0, 255, 0, 0.25)"
+        />
+        {markers.map((marker) => (
+          <Marker
+            key={marker.title}
+            coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+            title={marker.title}
+            description={marker.description}
+          >
+            <Image source={require('./assets/repairavatar.png')} style={{ height: 40, width: 40 }} />
+          </Marker>
+        ))}
+        
+      </MapView>
+    </View>
   );
 };
 
